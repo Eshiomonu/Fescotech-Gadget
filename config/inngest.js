349ce23844195be21740d20244a5075f16fd4c62
kadirisunday <kadirisunday212@gmail.com>
2025-05-24 16:40:30 +0100

    Created inngest configuration and setup to manage data in database

---
+++ config/inngest.js
@@ -0,0 +1,26 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "fescotech" });
+
+// Innegest Function to save user data to a database
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  { event: "clerk/user.created" }
+  async ({event}) =>{
+    const {id, first_name, last_name, email_Addresses, image_url} = event.data
+    const userData = {
+      _id:id,
+      email:email_Addresses[0].email_address,
+      name:first_name+ ' '+ last_name,
+      imageUrl:image_url
+    }
+    await connectDB()
+    await User.create(userData)
+  }
+);
+
